df6bdd57d100c4e50d9f134122bdab1d6843a8d8
ViticoMC <victormanuelmartinezcampo178@gmail.com>
2025-03-28 17:02:52 -0400

    db

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
-import {initializeDatabase} from "./seeds/routes"
+// import {initializeDatabase} from "./seeds/routes"
 
 export default function Page(){
 
-    console.log("hola")
-    const {success, message} =  initializeDatabase()
+    // console.log("hola")
+    // const {success, message} =  initializeDatabase()
 
-    if(!success){
-        return <h1>{message}</h1>
-    }   
+    // if(!success){
+    //     return <h1>{message}</h1>
+    // }   
     return (
         <Link href='/home'>
         <h1>Hola desde Page</h1>
